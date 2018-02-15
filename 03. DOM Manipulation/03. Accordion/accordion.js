393fee424b0e4bffbfd03f677c543da3d9701b23
Krasimira Georgieva <krassi77bg@gmail.com>
2018-02-15 19:11:06 +0200

    Added task solution.

---
+++ 03. DOM Manipulation/03. Accordion/accordion.js
@@ -0,0 +1,14 @@
+function toggle() {
+    let extraDiv = document.getElementById('extra');
+    //let button = document.getElementsByClassName('button')[0];
+    let button = document.querySelectorAll('.head span.button')[0];
+    if(extraDiv.style.display === 'none'){
+        extraDiv.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        extraDiv.style.display = 'none';
+        button.textContent = 'More';
+    }
+}
+
+toggle();
