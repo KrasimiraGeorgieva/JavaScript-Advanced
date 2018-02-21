64f5b2289ca6521d6da73f97aab111316c93382a
Krasimira Georgieva <krassi77bg@gmail.com>
2018-02-21 23:01:25 +0200

    Added task solution.

---
+++ 05. jQuery Library/02. Timer/timer.js
@@ -0,0 +1,36 @@
+function timer() {
+    let hours = $('#hours');
+    let minutes = $('#minutes');
+    let seconds = $('#seconds');
+    let interval = null;
+    let startTimer = $('#start-timer');
+
+    startTimer.on('click', () => {
+        if (interval) {
+            clearInterval(interval);
+        }
+        interval = setInterval(updateTime, 1000);
+    });
+    $('#stop-timer').on('click', () => {
+        clearInterval(interval);
+        interval = null;
+    });
+
+    function updateTime() {
+        let secondsVal = seconds.text();
+        let minutesVal = minutes.text();
+        let hoursVal = hours.text();
+
+        seconds.text(`0${(+secondsVal + 1)}`.slice(-2));
+
+        if (secondsVal >= 59) {
+            seconds.text('00');
+            minutes.text(`0${(+minutesVal + 1)}`.slice(-2));
+            if (minutesVal >= 59) {
+                minutes.text('00');
+                hours.text(`0${(+hoursVal + 1)}`.slice(-2));
+            }
+        }
+    }
+}
+
