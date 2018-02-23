a20bef728540c69fa577e62c8092f21973993b8d
Krasimira Georgieva <krassi77bg@gmail.com>
2018-02-23 23:52:40 +0200

    Added task solution.

---
+++ 08. Lab Object Composition/03. List Processor.js
@@ -0,0 +1,21 @@
+function solve(arr) {
+   let commandExec = (function (){
+        let arr = [];
+        function add(str){
+            arr.push(str)
+        }
+        function remove(str){
+            arr = arr.filter(w=>w!== str)
+        }
+        function print(){
+            console.log(arr.join(','))
+        }
+        return {add,remove,print}
+    }());
+    for (let str of arr) {
+        let[command,value] = str.split(' ');
+        commandExec[command](value)
+    }
+}
+
+solve(['add hello', 'add again', 'remove hello', 'add again', 'print']);
