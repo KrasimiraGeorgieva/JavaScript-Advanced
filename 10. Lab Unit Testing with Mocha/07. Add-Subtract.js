141b52dab21aa96e69f5217ad10eebb159a5d41d
Krasimira Georgieva <krassi77bg@gmail.com>
2018-03-01 00:40:35 +0200

    Delete file.

--- 10. Lab Unit Testing with Mocha/07. Add-Subtract.js
+++ 10. Lab Unit Testing with Mocha/07. Add-Subtract.js
@@ -3,4 +3,13 @@
     //http://prntscr.com/ikh3zb
 //http://prntscr.com/ikh4go
 // http://prntscr.com/ikh5gx
+function createCalculator() {
+    let value = 0;
+    return {
+        add: function(num) { value += Number(num); },
+        subtract: function(num) { value -= Number(num); },
+        get: function() { return value; }
+    }
+}
 
+module.exports = createCalculator;
