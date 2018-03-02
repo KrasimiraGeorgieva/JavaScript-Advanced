f91177cbad5c87df211be5b0cd8ea0ab6eb0013d
Krasimira Georgieva <krassi77bg@gmail.com>
2018-03-02 21:52:01 +0200

    Added task solution.

---
+++ 12. Lab Classes and Members/04. Circle.js
@@ -0,0 +1,23 @@
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    set diameter(newDiameter) {
+        this.radius = newDiameter / 2 ;
+    }
+
+    get area(){
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+let c = new Circle(10);
+c.diameter = 10;
+console.log(c.area);
+c.diameter = 5;
+console.log(c.diameter);
