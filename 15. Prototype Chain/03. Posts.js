516daeb1f9eada18ce736c83ff47a3a131c0ecd6
Krasimira Georgieva <krassi77bg@gmail.com>
2018-03-15 23:47:20 +0200

    Added task solution.

---
+++ 15. Prototype Chain/03. Posts.js
@@ -0,0 +1,81 @@
+function solve() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+            return `Post: ${this.title}\n` + `Content: ${this.content}`;
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = Number(likes);
+            this.dislikes = Number(dislikes);
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            let smp = super.toString() + `\nRating: ${this.likes - this.dislikes}`;
+            if (this.comments.length > 0) {
+                smp = smp + '\nComments:';
+                this.comments.forEach(c => smp += `\n * ${c}`);
+            }
+
+            return smp;
+        }
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = Number(views);
+        }
+
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString(){
+            return super.toString() + `\nViews: ${this.views}`;
+        }
+    }
+
+    return {Post, SocialMediaPost, BlogPost}
+}
+
+// Input/Output
+
+let post = new Post("Post", "Content");
+
+console.log(post.toString());
+
+// Post: Post
+// Content: Content
+
+let scm = new SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
+// Post: TestTitle
+// Content: TestContent
+// Rating: -5
+// Comments:
+//  * Good post
+//  * Very good post
+//  * Wow!
+
+
+
