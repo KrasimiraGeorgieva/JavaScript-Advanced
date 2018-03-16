c08ccc00ace0bdb89c0389afda591d94d9b8f939
Krasimira Georgieva <krassi77bg@gmail.com>
2018-03-16 23:20:04 +0200

    Added task solution.

---
+++ 16. Lab Modules/04. Inheritance/Entity.js
@@ -0,0 +1,11 @@
+class Entity {
+    constructor(name) {
+        if(new.target === Entity){
+            throw new TypeError('Abstract file')
+        }
+        this.name = name;
+    }
+}
+
+module.exports = Entity;
+
